c68502ba44c5ceaec3cd2774841a02e6257b3b6a
WissamKahwaji <kahwajiwisam@gmail.com>
2023-03-01 01:51:13 +0200

    complete app

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {  BsGithub, BsLinkedin } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <a href="https://github.com/WissamKahwaji" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
+    </div>
+    <div>
+      <a href="https://www.linkedin.com/in/wissam-kahwaji-58132922a/" target="_blank" rel="noreferrer" >
+        <BsLinkedin />
+      </a>
+    </div>
+    <div>
+    <a href="https://www.facebook.com/wesam.kahwaje" target="_blank" rel="noreferrer" >
+        <FaFacebookF />
+      </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
